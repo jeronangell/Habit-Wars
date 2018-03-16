36c3c4776322364bffae965529c5641b1a302181
jeronangell <elder.angell@gmail.com>
2018-03-16 11:36:10 -0600

    fksad

---
+++ HabitWars/src/application/UserStats.java
@@ -0,0 +1,12 @@
+package application;
+
+public class UserStats {
+
+	public int goldreward;
+	public int mana;
+	public int exp;
+	public int perception;
+	public int intelligence;
+	public int constitution;
+	public int strength;
+}
---
+++ HabitWars/src/visualAspects/MenuActions.java
@@ -0,0 +1,12 @@
+package visualAspects;
+
+import application.TaskActionControler;
+
+public class MenuActions {
+	//TaskActionControler tak=new TaskActionControler();
+	public void logout() {
+		System.out.println("logging out");
+		//tak.reset();
+	}
+
+}
